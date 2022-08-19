750e00c0554905ac4e39a6e248f9b23b30cf8a7a
ThaooNguyenn <ronnguyen2862@gmail.com>
2022-08-19 09:10:27 +0700

    update.

--- assets/js/header.js
+++ assets/js/header.js
@@ -17,38 +17,11 @@ window.onclick = function(event) {
     }
   }
 
- 
-
   window.onscroll = function() {
-    const header = document.getElementsByTagName('header');
+    const header = document.getElementsByClassName('myHeader');
     if(window.scrollY >= 80 || window.pageYOffset >= 80) {
         header[0].classList.add("box-shadow");
     }else {
         header[0].classList.remove("box-shadow");
     }
 }  ;
-
-
-
-
-
-
-
-
-
-
-// const header = document.getElementById('nav')
-// const mobileMenu = document.getElementById("toggle")
-// var headerHeight = header.clientHeight;
-// // console.log('toggle');
-// mobileMenu.onclick = function () {
-//     // alert('thong bao')
-//     const isClosed = header.clientHeight === headerHeight;
-//     console.log(isClosed);
-//     if (isClosed) {
-//         header.style.height = 'auto';
-//     }
-//     else{
-//         header.style.height = null;
-//     }
-// }
